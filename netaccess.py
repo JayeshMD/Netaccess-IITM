b8f09d7f76119af49ec60ce2045f44492038093d
JayeshMD <jayeshmd2612@gmail.com>
2025-09-28 20:18:30 +0530

    Automating approving netaccess in IIT Madras.

--- netaccess.py
+++ netaccess.py
@@ -1,10 +1,10 @@
 #!path_to_python_bin/python3
+import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-import time
+from selenium.webdriver.chrome.service import Service
 
 #------------
 # Inputs
